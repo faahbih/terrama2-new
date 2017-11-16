ab29300a8d19e16ba7450f8467c9eafd9f28c7fb
Fabiana Oliveira <fabiana.rr.oliveira@gmail.com>
2017-11-16 15:34:01 -0200

    testing download page

---
+++ docs/download/count.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+
+	<?php
+
+	$hit_count = @file_get_contents('count_file.txt');
+	$hit_count++;
+	@file_put_contents('count_file.txt', $hit_count);
+
+	header('docs/download/terrama2-4.0.0-rc2-linux-x64-ubuntu-16.04.deb');
+
+	$hit_count = @file_get_contents('count_file.txt');
+	echo $hit_count;
+
+	?>
+	
+</body>
+</html>
+
+<!-- http://www.kavoir.com/2010/05/simplest-php-hit-counter-or-download-counter-count-the-number-of-times-of-access-visits-or-downloads.html -->
